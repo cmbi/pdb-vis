1b04fb2c87e2a29358c7f9f48ff06e24dfabc7a9
touwwouter <woutertouw@hotmail.com>
2014-09-08 13:46:36 +0200

    Fix #14: provide download link for YASARA. Add corresponding tests

--- tests/test_scenes.py
+++ tests/test_scenes.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from mock import patch
+from nose.tools import eq_, raises
 
 from pdb_vis.factory import create_app
 from pdb_vis.services import scenes
@@ -15,6 +16,56 @@ class TestScenes(object):
     def setup_class(cls):
         cls.flask_app = create_app({'TESTING': True})
         cls.app = cls.flask_app.test_client()
+
+    @raises(ValueError)
+    def test_get_scene_dir_invalid_scene(self):
+        name = '1crn-sym-contacts.sce'
+        scenes.get_scene_dir(None, None, name, None)
+
+    @raises(ValueError)
+    def test_get_scene_dir_invalid_scene_2(self):
+        name = '1crn_sym_contacts.sce'
+        scenes.get_scene_dir(None, None, name, None)
+
+    @raises(ValueError)
+    def test_get_scene_dir_invalid_scene_3(self):
+        name = '1crn_sym-contacts'
+        scenes.get_scene_dir(None, None, name, None)
+
+    @patch('os.path.isdir')
+    @patch('os.path.exists')
+    def test_get_scene_dir_exists(self, *args):
+        SCENES_ROOT = '/sce_root'
+        sce_type = 'ss2'
+        pdb_ac = '1crn'
+        expected_dir = os.path.join(SCENES_ROOT, sce_type, pdb_ac)
+        scene_dir = scenes.get_scene_dir('/sce_root', '1crn',
+                                         '1crn_sym-contacts.sce',
+                                         {'ss2': 'sym-contacts'})
+        eq_(scene_dir, expected_dir)
+
+    @raises(ValueError)
+    def test_get_scene_dir_doesnt_exist(self, *args):
+        SCENES_ROOT = '/sce_root'
+        sce_type = 'ss2'
+        pdb_ac = '1crn'
+        expected_dir = os.path.join(SCENES_ROOT, sce_type, pdb_ac)
+        scene_dir = scenes.get_scene_dir('/sce_root', '1crn',
+                                         '1crn_sym-contacts.sce',
+                                         {'ss2': 'sym-contacts'})
+        eq_(scene_dir, expected_dir)
+
+    @raises(ValueError)
+    @patch('os.path.isdir')
+    def test_get_scene_dir_file_doesnt_exist(self, *args):
+        SCENES_ROOT = '/sce_root'
+        sce_type = 'ss2'
+        pdb_ac = '1crn'
+        expected_dir = os.path.join(SCENES_ROOT, sce_type, pdb_ac)
+        scene_dir = scenes.get_scene_dir('/sce_root', '1crn',
+                                         '1crn_sym-contacts.sce',
+                                         {'ss2': 'sym-contacts'})
+        eq_(scene_dir, expected_dir)
 
     @patch('os.path.exists')
     def test_find_exists(self, mock_exists):
