875d8702692ed5954888512b5322dd772cfc2dc0
Jon Black <jon_black@mm.st>
2016-11-17 09:25:57 +0100

    Improve docker use (#21)
    Fixes #20

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,21 @@
-FROM python:2.7-onbuild
+FROM python:2.7
 
+# dependencies
 RUN apt-get install -y curl
 RUN curl https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 
-RUN . ~/.nvm/nvm.sh ; nvm install v4.4.0 ; npm install -g bower ; bower install --allow-root
-
+# pdb-vis
+RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
+
+COPY bower.json /usr/src/app/
+COPY .bowerrc /usr/src/app/
+RUN . ~/.nvm/nvm.sh && nvm install v4.4.0 && \
+    npm install -g bower && bower install --allow-root
+
+COPY requirements.txt /usr/src/app/
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . /usr/src/app
+
+# settings
 EXPOSE 6015
--- dev_settings.py
+++ dev_settings.py
@@ -5,18 +5,3 @@ SECRET_KEY = 'development_key'
 # Debug toolbar
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
-
-# Email logging settings
-MAIL_SERVER = "smtp.umcn.nl"
-MAIL_SMTP_PORT = 25
-MAIL_FROM = "pdb-vis@cmbi.umcn.nl"
-MAIL_TO = ["Jon.Black@radboudumc.nl"]
-
-# Databank paths
-ACC_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/acc/'
-ACC_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/acc/'
-DSP_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/dsp/'
-DSP_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/dsp/'
-SCE_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/scenes/'
-SCE_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/scenes/'
-SCE_TYPES = {'ss2': 'sym-contacts', 'iod': 'ion-sites'}
