742f773d05c0c4144196d0ea5200eabff8313f82
Jon Black <jon_black@mm.st>
2016-11-17 09:25:08 +0100

    Improve docker use
    Fixes #20

---
+++ pdb_vis/default_settings.py
@@ -0,0 +1,14 @@
+# Email logging settings
+MAIL_SERVER = "smtp.umcn.nl"
+MAIL_SMTP_PORT = 25
+MAIL_FROM = "pdb-vis@cmbi.umcn.nl"
+MAIL_TO = ["Jon.Black@radboudumc.nl", "Coos.Baakman@radboudumc.nl"]
+
+# Databank paths
+ACC_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/acc/'
+ACC_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/acc/'
+DSP_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/dsp/'
+DSP_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/dsp/'
+SCE_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/scenes/'
+SCE_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/scenes/'
+SCE_TYPES = {'ss2': 'sym-contacts', 'iod': 'ion-sites'}
