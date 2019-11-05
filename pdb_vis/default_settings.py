b9adcbe78ddbe3ec71bdc087ed267c08022b9814
Coos Baakman <cbaakman@radboudumc.nl>
2019-11-05 10:49:39 +0100

    the path has changed

--- pdb_vis/default_settings.py
+++ pdb_vis/default_settings.py
@@ -5,10 +5,10 @@ MAIL_FROM = "pdb-vis@cmbi.umcn.nl"
 MAIL_TO = ["Jon.Black@radboudumc.nl", "Coos.Baakman@radboudumc.nl"]
 
 # Databank paths
-ACC_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/acc/'
+ACC_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb-old/acc/'
 ACC_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/acc/'
-DSP_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/dsp/'
+DSP_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb-old/dsp/'
 DSP_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/dsp/'
-SCE_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb/scenes/'
+SCE_ROOT_PDB = '/mnt/cmbi4/wi-lists/pdb-old/scenes/'
 SCE_ROOT_PDB_REDO = '/mnt/cmbi4/wi-lists/redo/scenes/'
 SCE_TYPES = {'ss2': 'sym-contacts', 'iod': 'ion-sites'}
